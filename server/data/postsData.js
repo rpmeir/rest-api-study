8eb65eabe51c7e66e90103310443b4b2794e2539
Rodrigo Pires Meira <rpmeir@gmail.com>
2021-08-17 00:50:57 -0300

    Conclusao dos metodos

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -2,4 +2,24 @@ const database = require('../infra/database');
 
 exports.getPosts = function () {
     return database.query('SELECT * FROM blog.post');
-}
+};
+
+exports.getPost = function (id) {
+    return database.oneOrNone('SELECT * FROM blog.post WHERE id = $1', [id]);
+};
+
+exports.getPostByTitle = function (title) {
+    return database.oneOrNone('SELECT * FROM blog.post WHERE title = $1', [title]);
+};
+
+exports.savePost = function (post) {
+    return database.one('INSERT INTO blog.post (title, content) VALUES ($1, $2) RETURNING *', [post.title, post.content]);
+};
+
+exports.updatePost = function (id, post) {
+    return database.none( 'UPDATE blog.post SET title = $1, content = $2 WHERE id = $3', [post.title, post.content, id] );
+};
+
+exports.deletePost = function (id) {
+    return database.none('DELETE FROM blog.post WHERE id = $1', [id]);
+};
